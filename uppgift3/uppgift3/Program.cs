fba75c94d256d9b9f769bd96f8e2b55fddd2f922
Vilgot <vilu02001@utb.vaxjo.se>
2019-10-04 09:10:42 +0200

    uppg 3 klar

--- uppgift3/uppgift3/Program.cs
+++ uppgift3/uppgift3/Program.cs
@@ -6,11 +6,23 @@ namespace uppgift3
     {
         static void Main(string[] args)
         {
-            double[] temperatur = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9 };
-            Array.Sort(temperatur);
-            if (temperatur.Length % 2 == 0)
+            double[] temperatur = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9};
+            Console.WriteLine("Medianen är " + Median(temperatur));
+        }
+        public static double Median(double[] a)
+        {
+            Array.Sort(a);
+            if (a.Length % 2 != 0)
             {
-                temperatur.Length
+                int i = a.Length / 2;
+                return a[i];
+            }
+            else
+            {
+                int j = a.Length / 2;
+                int k = (a.Length / 2) - 1;
+                double l = (a[j] + a[k]) / 2;
+                return l;
             }
         }
     }
